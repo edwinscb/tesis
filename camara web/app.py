b9aa881fe58430cbd97f48e75b9a3042eb3985d6
EdwinPc <edwinsantiagocastrobuitrago@gmail.com>
2024-11-20 16:12:48 -0500

    update pag web camara cliente

--- camara web/app.py
+++ camara web/app.py
@@ -7,4 +7,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('D:/tesis/server.crt', 'D:/tesis/server.key'))
+    # Utiliza las rutas de los certificados SSL correctas
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('D:\\tesis\\server.crt', 'D:\\tesis\\server.key'))
