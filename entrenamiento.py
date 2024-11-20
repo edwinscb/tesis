8bd27cb8c56ee18f6738cd8d7f8b3a0d0334af08
EdwinPc <edwinsantiagocastrobuitrago@gmail.com>
2024-11-20 10:33:38 -0500

    entrenamiento yolo11l epochs=100,imgsz=640, device=0,batch=5, half=True

--- entrenamiento.py
+++ entrenamiento.py
@@ -2,12 +2,13 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO("yolo11m.pt")
+    model = YOLO("yolo11l.pt")
 
     train_results = model.train(
         data="YOLODataset/dataset.yaml",  # path to dataset YAML
         epochs=100,  # number of training epochs
         imgsz=640,  # training image size
         device=0,  # use the first GPU
-        batch=32,  # specify batch size
+        batch=5,  # specify batch size
+        half=True,
     )
