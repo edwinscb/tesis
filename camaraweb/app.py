4db3f54a14d7d55a3f52c8b9f707048372d368d6
EdwinPc <edwinsantiagocastrobuitrago@gmail.com>
2024-11-27 00:21:28 -0500

    update botones

--- camaraweb/app.py
+++ camaraweb/app.py
@@ -3,70 +3,44 @@ from io import BytesIO
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 from PIL import Image
-import io
 import cv2
 from ultralytics import YOLO
-import numpy as np  # Necesario para convertir el ndarray a imagen
+import numpy as np
 
-# Configuración de Flask y SocketIO
 app = Flask(__name__)
-model = YOLO("D:/tesis/runs/detect/yolo11l/weights/best.pt")  # Cargar el modelo una sola vez
+model = YOLO("D:/tesis/runs/detect/yolo11l/weights/best.pt")
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Tu archivo HTML donde se carga el video
+    return render_template('index.html')
 
 @socketio.on('start_video')
 def handle_video_frame(data):
     try:
-        # Extrae la cadena base64 del fotograma
         base64_string = data['frame']
         img_data = base64.b64decode(base64_string.split(',')[1])
-        
-        # Decodifica la imagen
-        image = Image.open(BytesIO(img_data))
-
-        # Asegúrate de que la imagen esté en modo RGB
-        image = image.convert('RGB')
-
-        # Realiza la detección con el modelo YOLO
-        results = model(image)  # Detecta objetos en la imagen
-
-        # Filtrar las detecciones con un umbral de confianza > 0.5
-        detections = []
-        for result in results:
-            for box in result.boxes:
-                conf = box.conf[0].item()  # Confianza de la detección
-                if conf > 0.5:  # Filtro de confianza
-                    detections.append({
-                        'box': box.xyxy[0].tolist(),  # Coordenadas de la caja delimitadora
-                        'confidence': conf,
-                        'class_id': int(box.cls[0].item())  # Clase detectada
-                    })
-
-        # Procesar la imagen: dibujar resultados solo para detecciones con alta confianza
-        annotated_image = results[0].plot()  # Añade las anotaciones de detección
-
-        # Convertir el ndarray a RGB (si es necesario, depende del modelo)
+        image = Image.open(BytesIO(img_data)).convert('RGB')
+        results = model(image)
+        detections = [
+            {
+                'box': box.xyxy[0].tolist(),
+                'confidence': box.conf[0].item(),
+                'class_id': int(box.cls[0].item())
+            }
+            for result in results for box in result.boxes if box.conf[0].item() > 0.5
+        ]
+        annotated_image = results[0].plot()
         annotated_image_rgb = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
-
-        # Voltear la imagen horizontalmente (modo espejo)
         mirrored_image = cv2.flip(annotated_image_rgb, 1)
-        # Convertir el ndarray RGB a una imagen de PIL
         pil_image = Image.fromarray(mirrored_image)
-
-        # Convierte la imagen procesada nuevamente a base64
         buffered = BytesIO()
         pil_image.save(buffered, format="JPEG")
         processed_image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
-        # Envía el fotograma procesado al cliente
         emit('video_frame', {'frame': processed_image_base64})
-
     except Exception as e:
         print(f"Error al procesar el fotograma: {e}")
-        emit('video_frame', {'frame': None})  # Enviar None si hubo un error
+        emit('video_frame', {'frame': None})
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000, ssl_context=('D:\\tesis\\server.crt', 'D:\\tesis\\server.key'))
--- camaraweb/static/script.js
+++ camaraweb/static/script.js
@@ -1,97 +1,83 @@
-// Establece conexión al servidor mediante WebSocket
 const socket = io();
 
-// Referencias a elementos del DOM
+
 const canvas = document.getElementById("canvas");
+
 const ctx = canvas.getContext("2d");
 const changeCameraButton = document.getElementById("change-camera");
+const fullscreenButton = document.getElementById("fullscreenButton");
 
-// Variable para almacenar la cámara actual
+
 let currentCameraId = 0;
 let stream;
 
-// Función para obtener las cámaras disponibles
 async function getCameras() {
     try {
         const devices = await navigator.mediaDevices.enumerateDevices();
-        const videoDevices = devices.filter(device => device.kind === 'videoinput');
-        return videoDevices;
+        return devices.filter(device => device.kind === 'videoinput');
     } catch (err) {
         console.error("Error al obtener las cámaras:", err);
         return [];
     }
 }
 
-// Configura la cámara y comienza la transmisión (pero no mostrar el video)
 async function initializeCamera(cameraId) {
     try {
-        // Detener la cámara actual si ya está en uso
         if (stream) {
-            const tracks = stream.getTracks();
-            tracks.forEach(track => track.stop());
+            stream.getTracks().forEach(track => track.stop());
         }
 
         const videoDevices = await getCameras();
         const constraints = { video: { deviceId: videoDevices[cameraId].deviceId } };
         stream = await navigator.mediaDevices.getUserMedia(constraints);
 
-        // Inicia la captura y envío de fotogramas, pero no mostrar el video
-        captureAndSendFrames(stream); // Inicia la captura y envío de fotogramas
+        captureAndSendFrames(stream);
     } catch (err) {
         console.error("Error al acceder a la cámara:", err);
     }
 }
 
-// Cambia entre las cámaras disponibles
 async function changeCamera() {
-    currentCameraId = (currentCameraId + 1) % (await getCameras()).length; // Cambia de cámara
+    const cameras = await getCameras();
+    currentCameraId = (currentCameraId + 1) % cameras.length;
     initializeCamera(currentCameraId);
 }
 
-// Captura y envía fotogramas al servidor
 function captureAndSendFrames(stream) {
     const imageCapture = new ImageCapture(stream.getVideoTracks()[0]);
-
-    let errorCount = 0; // Contador de errores consecutivos
-    const maxErrors = 5; // Máximo de intentos fallidos antes de detenerse
-    const baseRetryDelay = 500; // Tiempo base entre reintentos (en ms)
+    let errorCount = 0;
+    const maxErrors = 5;
+    const baseRetryDelay = 500;
 
     const captureFrame = async () => {
         try {
             const imageBitmap = await imageCapture.grabFrame();
-            if (!imageBitmap) throw new Error("Fotograma vacío");
-
-            // Convertir fotograma a Base64
             const base64Frame = convertFrameToBase64(imageBitmap);
+
             if (base64Frame) {
                 socket.emit('start_video', { frame: base64Frame });
-                errorCount = 0; // Reiniciar contador si la captura es exitosa
+                errorCount = 0;
             }
 
-            setTimeout(captureFrame, 40); // Repite cada 45 ms
-
+            setTimeout(captureFrame, 100);
         } catch (err) {
             console.error("Error al capturar fotograma:", err);
-
-            errorCount += 1;
+            errorCount++;
 
             if (errorCount > maxErrors) {
-                console.warn("Se alcanzó el límite de intentos fallidos. Deteniendo captura.");
-                return; // Detener la captura si se exceden los errores permitidos
+                console.warn("Límite de intentos fallidos alcanzado. Deteniendo captura.");
+                return;
             }
 
-            // Retraso progresivo en caso de errores continuos
-            const retryDelay = baseRetryDelay * errorCount; // Aumentar el retraso según el número de errores
+            const retryDelay = baseRetryDelay * errorCount;
             console.log(`Reintentando en ${retryDelay} ms...`);
             setTimeout(captureFrame, retryDelay);
         }
     };
 
-    captureFrame(); // Llama por primera vez
+    captureFrame();
 }
 
-
-// Convierte un fotograma (ImageBitmap) en una cadena Base64
 function convertFrameToBase64(imageBitmap) {
     const tempCanvas = document.createElement('canvas');
     tempCanvas.width = imageBitmap.width;
@@ -103,30 +89,36 @@ function convertFrameToBase64(imageBitmap) {
     return tempCanvas.toDataURL('image/jpeg');
 }
 
-// Dibuja un fotograma procesado en el canvas
 function displayProcessedFrame(base64Frame) {
     const img = new Image();
     img.src = `data:image/jpeg;base64,${base64Frame}`;
 
     img.onload = () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpia el canvas
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height); // Dibuja la nueva imagen
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     };
 }
 
-// Escucha los fotogramas procesados desde el servidor
+function toggleFullscreen() {
+    if (canvas.requestFullscreen) {
+        canvas.requestFullscreen();
+    } else if (canvas.mozRequestFullScreen) { 
+        canvas.mozRequestFullScreen();
+    } else if (canvas.webkitRequestFullscreen) { 
+        canvas.webkitRequestFullscreen();
+    } else if (canvas.msRequestFullscreen) { 
+        canvas.msRequestFullscreen();
+    }
+}
+
 socket.on('video_frame', (data) => {
     if (data.frame) {
-        console.log("Fotograma procesado recibido", data.frame);
         displayProcessedFrame(data.frame);
     } else {
         console.warn("Fotograma vacío recibido del servidor");
-        
     }
 });
 
-// Inicializa la cámara al cargar el script
 initializeCamera(currentCameraId);
-
-// Asocia la acción de cambiar de cámara al botón
 changeCameraButton.addEventListener("click", changeCamera);
+fullscreenButton.addEventListener("click", toggleFullscreen);
