c46621adfd15299e63c3e77b1359600ff20ed183
EdwinPc <edwinsantiagocastrobuitrago@gmail.com>
2024-11-27 16:22:39 -0500

    update

--- SmartVolley/src/app/app.component.spec.ts
+++ SmartVolley/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
