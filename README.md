e15fd5cfa8b517095406d78ef85470ae34e1164b
EdwinPc <edwinsantiagocastrobuitrago@gmail.com>
2024-11-28 14:02:49 -0500

    organizacion carpetas

--- README.md
+++
@@ -1,5 +0,0 @@
-# tesis
-
-opencv
-yolo
-ip webcam camara del celular por wifi
--- pruebaApi.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/predict_camera/"
-response = requests.post(url)
-print("Detecciones:", response.json())
